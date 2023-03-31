47a314bf3eeaf9c8909801cc9f74226f217881f2
mostafa mohamed <mousamostafa00@gmail.com>
2023-03-31 17:20:23 +0200

    part task

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
+import 'home/homeScreen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -22,44 +24,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int red = 150;
-  int green = 150;
-  int blue = 150;
-
-  void changeColor() {
-    var r = Random();
-    red = r.nextInt(255);
-    green = r.nextInt(255);
-    blue = r.nextInt(255);
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromRGBO(red, green, blue, 1),
-      body: InkWell(
-        onTap: () {
-          changeColor();
-        },
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: const Center(
-            child: Text("Hello There"),
-          ),
-        ),
-      ),
-    );
-  }
-}
